cf284266d409703669c7fc57fb7146d9eff196de
Gordon Doskas <gordon2012@users.noreply.github.com>
2019-05-09 22:03:02 -0700

    Linking Between Pages with Gatsby
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const AboutPage = () => {
   return (
     <div>
       <h1>About Me</h1>
       <p>I am legend.</p>
+      <p>
+        You can contact me <Link to="/contact">here</Link>.
+      </p>
     </div>
   );
 };
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,13 @@ const ContactPage = () => {
   return (
     <div>
       <h1>Contact Me</h1>
-      <p>Send me an electronic letter or a raven.</p>
+      <p>
+        Send me a raven via{' '}
+        <a href="https://twitter.com/gordondoskas" target="_blank">
+          @gordondoskas
+        </a>{' '}
+        on Twitter.
+      </p>
     </div>
   );
 };
