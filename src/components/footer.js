eb5a4c68986cbf4dc027c4cf7c1a5acd507397d3
Gordon Doskas <gordon2012@users.noreply.github.com>
2019-05-10 20:09:27 -0700

    Gatsby Data with GraphQL
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `);
   return (
     <footer>
-      <p>Copyright 2019 Gordon Doskas</p>
+      <p>Copyright 2019 {data.site.siteMetadata.author}</p>
     </footer>
   );
 };
